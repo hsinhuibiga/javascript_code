badd436dafba27facbd9a123f5c3d245def0fd7d
hsinhuibiga <hsinhuibiga@gmail.com>
2022-02-22 09:46:07 +0800

    Add files via upload

---
+++ excel_sheet_column_number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+  var res = 0;
+  var num = 0;
+  var len = columnTitle.length;
+  for (var i = 0; i < len; i++) {
+    num = getNum(columnTitle[len - 1 - i]);
+    res += Math.pow(26, i) * num;
+  }
+  return res;
+};
