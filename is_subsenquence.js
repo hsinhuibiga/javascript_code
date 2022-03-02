e3f3879a51c93350c94f1126dd289e57e62e733d
hsinhuibiga <hsinhuibiga@gmail.com>
2022-03-02 10:13:39 +0800

    Add files via upload

---
+++ is_subsenquence.js
@@ -0,0 +1,16 @@
+const isSubsequence = (s, t) =>{
+  const max = s.length
+  return isSub(0,0)
+  function isSub (sIndex, startIndex){
+    if (sIndex=== max){
+	  return true
+	}
+ 
+    const index = t.indexOf(s[sIndex], startIndex)
+    if (index < 0){	
+	  return false
+	}
+	
+	return isSub(sIndex + 1, index +1)
+   }
+}
